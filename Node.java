b5f0f7dc1cb5bcc7cf535fe4cc4ec5c1df7755e0
Sharon Shin <89420953+sharonmshinn@users.noreply.github.com>
2021-11-30 13:54:54 -0600

    Update Node.java

--- Node.java
+++ Node.java
@@ -9,9 +9,13 @@ public class Node{
 	String abbrev;  // The abbreviation for the Node
 	ArrayList<Edge> outgoingEdges;  
 	ArrayList<Edge> incomingEdges;
+	//implemented color variable
 	String color;
+	//implemented discovery time
 	int discovery;
+	//implemented finishing time
 	int finish;
+	//implemented previous/parent node
 	Node previous;
 	
 	
@@ -22,29 +26,36 @@ public class Node{
 		name = null;
 		outgoingEdges = new ArrayList<Edge>();
 		incomingEdges = new ArrayList<Edge>();
+		//color set as WHITE to show that it has not been discovered yet to start
 		color = "WHITE";
+		//both discovery and finish time at 0 to start
 		discovery = 0;
 		finish = 0;
+		//null previous/parent node to start
 		previous = null;
 	}
 	
-	
+	//implemented an equals function to determine if the names are equal
 	public boolean equals(Node other) {
 		return this.getName().equals(other.getName());
 	}
 	
+	//implemented a getter to return the previous/parent node
 	public Node getPrevious() {
 		return previous;
 	}
 	
+	//implemented a getter to return the color of a node
 	public String getColor() {
 		return color;
 	}
 	
+	//implemented a getter to return the discovery time of a node
 	public int getDiscovery() {
 		return discovery;
 	}
 	
+	//implemented a getter to return a finish time of a node
 	public int getFinish() {
 		return finish;
 	}
@@ -74,14 +85,17 @@ public class Node{
 		previous = thePrevious;
 	}
 	
+	//implemented a setter to set a color of a node
 	public void setColor(String theColor) {
 		color = theColor;
 	}
 	
+	//implemented a setter to set a discovery time of a node
 	public void setDiscovery(int theDiscovery) {
 		discovery = theDiscovery;
 	}
 	
+	//implemented a setter to set a finish time of a node
 	public void setFinish(int theFinish) {
 		finish = theFinish;
 	}
@@ -105,30 +119,30 @@ public class Node{
 	public void addIncomingEdge( Edge e ) {
 		incomingEdges.add( e );
 	}
-
+	
+	//print function to print the abbrev of a node and its associated incoming edges
 	public String printIn() {
 		String output = "";
 		output += "Node " + this.getAbbrev() + " has indegree " + this.getIncomingEdges().size() + ".";
 		return output;
 	}
 	
+	//print function to print the abbrev of a node and its associated outgoing edges
 	public String printOut() {
 		String output = "";
 		output += "Node " + this.getAbbrev() + " has outdegree " + this.getOutgoingEdges().size() + ".";
 		return output;
 	}
 	
-	//public String toString() {
-	//	return abbrev;
-	//}
 	
+	//print function toString to return the node's abbrev and its discovery and finish times
 	public String toString() {
 		String output = "";
 		output += this.getAbbrev() + "\t" + this.getDiscovery() + "\t" + this.getFinish();
 		return output;
 	}
 	
-	
+	//print function to return the node's abbrev
 	public String printNode() {
 		String output = "";
 		output += this.getAbbrev();
